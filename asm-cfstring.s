cf9fe04c080c609efb909c5940cde3004568e466
Jeremy Legendre <jslegendre@gmail.com>
2018-01-25 16:27:50 -0500

    Update asm-cfstring.s

--- asm-cfstring.s
+++ asm-cfstring.s
@@ -8,7 +8,7 @@ mov $0x2000001, %rax
 syscall # exit(0)
 
 .section __TEXT,__cstring
-CString: .asciz	 "what am i doing with my life plz help me"		#More info on '.asciz' for those who want it: docs.huihoo.com/redhat//rhel-4-docs/rhel-as-en-4/asciz.html
+CString: .asciz	 "what am i doing with my life plz help me"  #More info on '.asciz' for those who want it: docs.huihoo.com/redhat//rhel-4-docs/rhel-as-en-4/asciz.html
 
 .section __DATA,__cfstring
 CString_cfstring:
@@ -16,4 +16,4 @@ CString_cfstring:
 	.long	1992                    
 	.space	4
 	.quad	CString 	# Name of cstring variable from section above								
-	.quad	41 			# Basically strlen(CString)
+	.quad	41 		# Basically strlen(CString)
